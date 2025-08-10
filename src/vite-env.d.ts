12b2202d444b858b948977108c219a978c3bbacf
Karan <ikarn.dev@gmail.com>
2025-08-10 10:53:27 +0530

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,41 @@
+/// <reference types="vite/client" />
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.less" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.styl" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.stylus" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.pcss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.sss" {
+  const content: string;
+  export default content;
+}
